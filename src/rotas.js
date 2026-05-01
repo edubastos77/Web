3b0e3c200bef3eec02d0ee175b04ead774fe2a54
edubastos77 <edubastosmail@gmail.com>
2026-05-01 05:28:45 -0300

    Update do package.json

--- src/rotas.js
+++ src/rotas.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './paginas/Login';
 import Cadastro from './paginas/Cadastro';
@@ -7,15 +7,15 @@ import NotFound from './paginas/NotFound';
 
 const Rotas = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route path="/" element={<Login/>} />
-                <Route path="/cadastro" element={<Cadastro/>} />
-                <Route path="/principal" element={<Principal/>} />
-                <Route path="*" element={<NotFound/>} />
+                <Route exact path="/" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/principal" element={<Principal />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
-        </BrowserRouter>
-    )
+        </HashRouter>
+    );
 }
 
 export default Rotas;
